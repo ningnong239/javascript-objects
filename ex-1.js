240226dcf9c394fd5d839b65fc73a023bc5d70a8
ningnong239 <ningnong2539@gmail.com>
2025-07-17 13:45:48 +0700

    feat: update food order and product details, add launch configuration

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,13 @@
 // Exercise #1: Food Order
+let foodOrder = {
+    orderNumber: "A0234",
+    address: "Bangkok",
+    paymentType: "Cash",
+    totalPrice: 4500,
+    restaurantName: "MK"
+};
+foodOrder.totalPrice = 5000;
+foodOrder.paymentType = "Credit Card";
 
 // Start coding here
+console.log(foodOrder)
